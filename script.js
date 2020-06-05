618da732a8f5eec2ec12319c7cf739aff009ef59
Rishes lena <leenuun@gmail.com>
2020-06-05 21:28:36 +0700

    Name of commit

--- script.js
+++
@@ -1,9 +0,0 @@
-// alert('всем пис');
-
-// var money = 25;
-// var income =32;
-// var addExpenses = 45;
-// var deposit = 55;
-// var mission = 45;
-// var period = 76;
-console.log(5); 
---
+++ scriptt.js
@@ -0,0 +1,20 @@
+var money = 50000; //Доход в месяц
+console.log(typeof money);
+var income = 'хореограф'; //доп.доход
+console.log(typeof income); 
+var addExpenses = 'интернет, еда, проезд';//расходы
+console.log(addExpenses.length);
+var deposit = true;
+console.log(typeof deposit);
+var mission = 1000000; //стремление к накоплению
+var period = 5; // сроки
+console.log( 'Период равен', period, 'месяцев' );
+console.log( 'Цель заработать', mission, 'рублей' );
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(''));
+var budgetDay;
+budgetDay =6666;
+console.log('Ты должен зарабатывать ', budgetDay, 'рублей')
+
+
+
